925c3295949ac8b1c64561d2af00baed2ab1c1b4
Aliu6991 <aliuolamide18@gmail.com>
2025-06-11 08:06:54 -0400

    Create STREAM_LIT.py

---
+++ STREAM_LIT.py
@@ -0,0 +1,28 @@
+import math
+import streamlit as st
+
+st.markdown("HELLO")
+st.markdown("# HELLO")
+
+
+
+
+
+
+
+
+
+
+'''
+
+power =math.pow(7,10)
+
+cosine =math.cos(45)
+print(cosine)
+
+
+inv_cos = math.acos(0.5253219888177297)
+print(inv_cos)
+
+st.write(cosine)
+'''
